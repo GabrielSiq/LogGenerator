16d0fb776b4c9897c4d2a69dd5ec54a994b2c0d4
Gabriel Siqueira <gabrielsiq@msn.com>
2019-04-23 16:39:21 -0700

    Changed parsing of activity to parse fields first.

--- model_builder.py
+++ model_builder.py
@@ -36,15 +36,22 @@ class ModelBuilder:
         return container
 
     @classmethod
-    def _parse_activity(cls, activity):
-        activity_id = activity.get('id')
+    def _parse_activity(cls, activity_child):
+        activity_fields = cls._parse_activity_fields(activity_child)
 
-        name = activity.find('Name').text
+        return Activity(id=activity_fields.get('id'), name=activity_fields.get('name'), distribution=activity_fields.get('distribution', 0), data_input=activity_fields.get('data_input'), data_output=activity_fields.get('data_output'), resources=activity_fields.get('resources'), failure_rate=activity_fields.get('failure_rate', 0), retries=activity_fields.get('retries', 0), timeout=activity_fields.get('timeout'), priority=activity_fields.get('priority', 'normal'))
 
-        distribution = cls._parse_distribution(activity.find('Duration/Distribution'))
+    @classmethod
+    def _parse_activity_fields(cls, activity_child):
+        fields = dict()
+        fields['id'] = activity_child.get('id')
+
+        fields['name'] = activity_child.find('Name').text
+
+        fields['distribution'] = cls._parse_distribution(activity_child.find('Duration/Distribution'))
 
         data_input = []
-        data_input_child = activity.find('DataInput')
+        data_input_child = activity_child.find('DataInput')
         # TODO: Add some checking to verify that the children objects are what we think they are.
         if data_input_child is not None:
             for data_object in data_input_child:
@@ -53,9 +60,10 @@ class ModelBuilder:
                     # TODO: better way to handle custom exception
                     raise AttributeError('Missing data object id.')
                 data_input.append(data_object.get('id'))
+        fields['data_input'] = data_input
 
         data_output = []
-        data_output_child = activity.find('DataOutput')
+        data_output_child = activity_child.find('DataOutput')
 
         if data_output_child is not None:
             for data_object in data_output_child:
@@ -65,9 +73,10 @@ class ModelBuilder:
                     raise AttributeError('Missing data object id.')
 
                 data_output.append(data_object.get('id'))
+        fields['data_output'] = data_output
 
         resources = []
-        resources_child = activity.find('Resources')
+        resources_child = activity_child.find('Resources')
 
         if resources_child is not None:
             for resource in resources_child:
@@ -78,43 +87,47 @@ class ModelBuilder:
                     resources.append(res)
                 except AttributeError:
                     print('Poorly formatted resource')
-
+        fields['resource'] = resources
         failure_rate = None
         retries = None
-        failure_child = activity.find('FailureRate')
+        failure_child = activity_child.find('FailureRate')
 
         if failure_child is not None:
             failure_rate = failure_child.text
             retries = failure_child.get('retries')
+        fields['failure_rate'] = failure_rate
+        fields['retries'] = retries
 
         # TODO: implement similar try/except methodology for others.
         try:
-            timeout = activity.find('Timeout').text
+            timeout = activity_child.find('Timeout').text
         except AttributeError:
             timeout = None
+        fields['timeout'] = timeout
 
         try:
-            priority = activity.find('Priority').text
+            priority = activity_child.find('Priority').text
         except AttributeError:
             priority = None
+        fields['priority'] = priority
 
-        return Activity(id=activity_id, name=name, distribution=distribution, data_input=data_input, data_output=data_output, resources=resources, failure_rate=failure_rate, retries=retries, timeout=timeout, priority=priority)
+        return fields
 
     @classmethod
-    def _parse_resource(cls, resource):
-        class_type = resource.get('type')
-        id = resource.get('id')
-        qty = resource.find('Quantity').text
+    def _parse_resource(cls, resource_child):
+        class_type = resource_child.get('type')
+        id = resource_child.get('id')
+        qty = resource_child.find('Quantity').text
 
         if class_type == 'human':
-            org = resource.find('Organization').text
-            dept = resource.find('Department').text
-            role = resource.find('Role').text
-            availability = cls._parse_availability(resource.find('Availability'))
+            org = resource_child.find('Organization').text
+            dept = resource_child.find('Department').text
+            role = resource_child.find('Role').text
+            availability = cls._parse_availability(resource_child.find('Availability'))
             res = HumanResource(id, org, dept, role, availability)
         elif class_type == 'physical':
-            type = resource.find('Type').text
-            delay = cls._parse_distribution(resource.find('Delay/Distribution'))
+            type = resource_child.find('Type').text
+            delay = cls._parse_distribution(resource_child.find('Delay/Distribution'))
             res = PhysicalResource(id, type, qty, delay)
         else:
             raise AttributeError('Poorly formatted resource.')
