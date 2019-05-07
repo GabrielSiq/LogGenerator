ba6919741e8659af7624929bf1e0e1efb95d25e8
Gabriel Siqueira <gabrielsiq@msn.com>
2019-05-07 14:31:01 -0700

    Added test model files and config file.

---
+++ config.py
@@ -0,0 +1,55 @@
+RESOURCE_TYPES = {
+    'physical': 'physical',
+    'human': 'human'
+}
+
+DAYS = {
+    0: 'Mon',
+    1: 'Tue',
+    2: 'Wed',
+    3: 'Thu',
+    4: 'Fri',
+    5: 'Sat',
+    6: 'Sun'
+}
+
+PRIORITY_TYPES = {
+    'low': 'low',
+    'normal': 'normal',
+    'high': 'high'
+}
+
+PRIORITY_VALUES = {
+    'low': 0,
+    'normal': 1,
+    'high': 2
+}
+
+GATEWAY_TYPES = {
+    'merge': 'merge',
+    'choice': 'choice',
+    'parallel': 'parallel'
+}
+
+DATA_TYPES = {
+    'form': 'form'
+}
+
+DEFAULT_PATHS = {
+    'activities': 'input/activities.xml',
+    'data':  'input/data.xml',
+    'models': 'input/models.xml',
+    'resources': 'input/resources.xml',
+    'log': 'output/'
+}
+
+FILE_ROOT = {
+    'activities': 'Activity',
+    'data':  'DataObject',
+    'models': 'Model',
+    'resources': 'Resource'
+}
+
+SUPPORTED_FORMATS = {
+    'json': 'json'
+}
